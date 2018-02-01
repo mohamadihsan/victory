1fa41acd81490428b760be3f89da82b2631fa546
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-02-01 10:50:38 +0700

    add menu pengiriman

---
+++ action/tampilkan_data_distribusi.php
@@ -0,0 +1,93 @@
+<?php
+// buka koneksi
+require_once '../config/connection.php';
+
+function Tanggal($tanggal) {
+    $BulanIndo = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+    $tahun = substr($tanggal, 0, 4);
+    $bulan = substr($tanggal, 5, 2);
+    $tgl = substr($tanggal, 8, 2);
+
+    $hasil = $tgl . " " . $BulanIndo[(int) $bulan - 1] . " " . $tahun;
+    return ($hasil);
+}
+
+$nomor_invoice = isset($_GET['nomor_invoice']) ? $_GET['nomor_invoice']: '';
+$nomor_invoice = trim($nomor_invoice);
+
+// sql statement
+if($nomor_invoice==''){
+    $sql = "SELECT d.nomor_invoice, d.status_persetujuan, d.tanggal_pengiriman, d.status_pengiriman,
+                  p.nama_konsumen, p.alamat
+            FROM pengiriman_produk d
+            LEFT JOIN pemesanan_produk pp ON pp.nomor_invoice=d.nomor_invoice
+            LEFT JOIN konsumen p ON p.id_konsumen=pp.id_konsumen
+            ORDER BY d.tanggal_pengiriman DESC";
+}else{
+    $sql = "SELECT d.nomor_invoice, d.status_persetujuan, d.tanggal_pengiriman, d.status_pengiriman,
+                  p.nama_konsumen, p.alamat
+            FROM pengiriman_produk d
+            LEFT JOIN pemesanan_produk pp ON pp.nomor_invoice=d.nomor_invoice
+            LEFT JOIN konsumen p ON p.id_konsumen=pp.id_konsumen
+            WHERE d.nomor_invoice = '$nomor_invoice'
+            ORDER BY d.tanggal_pengiriman DESC";
+}
+$result = mysqli_query($conn, $sql);
+$data = array();
+$no = 1;
+while ($row = mysqli_fetch_assoc($result)) {
+    $sub_array['no']                    = $no++;
+    $sub_array['nomor_invoice']          = $row['nomor_invoice'];
+    $sub_array['status_persetujuan']    = $row['status_persetujuan'];
+    $sub_array['tanggal_pengiriman']    = $row['tanggal_pengiriman'];
+    $sub_array['status_pengiriman']     = $row['status_pengiriman'];
+    $sub_array['nama_konsumen']        = $row['nama_konsumen'];
+    $sub_array['alamat']                = $row['alamat'];
+    $sub_array['action']	              = ' <a href="./index.php?menu=pemesanan&nomor_invoice='.$row['nomor_invoice'].'" class="btn btn-warning btn-xs"><i class="ace-icon fa fa-file-text-o bigger-120"></i> Detail</button>';
+
+    // ubah tampilan data
+    if ($sub_array['status_persetujuan'] == 'p') {
+        $sub_array['status_persetujuan'] = '<span class="label label-info label-white middle">
+                                                <i class="ace-icon fa fa-exclamation-triangle bigger-120"></i>
+                                                pending
+                                            </span>';
+    }else if ($sub_array['status_persetujuan'] == 's') {
+        $sub_array['status_persetujuan'] = '<span class="label label-success label-white middle">
+                                                <i class="ace-icon fa fa-check-square bigger-120"></i>
+                                                disetujui
+                                            </span>';
+    }else if ($sub_array['status_persetujuan'] == 't') {
+        $sub_array['status_persetujuan'] = '<span class="label label-danger label-white middle">
+                                                <i class="ace-icon fa fa-close bigger-120"></i>
+                                                ditolak
+                                            </span>';
+    }
+
+    if ($sub_array['status_pengiriman'] == 0) {
+        $sub_array['status_pengiriman'] = '<span class="label label-warning label-white middle">
+                                                <i class="ace-icon fa fa-exclamation-triangle bigger-120"></i>
+                                                belum diterima
+                                            </span>';
+    }else{
+        $sub_array['status_pengiriman'] = '<span class="label label-success label-white middle">
+                                                <i class="ace-icon fa fa-check-square bigger-120"></i>
+                                                sudah diterima
+                                            </span>';
+    }
+
+    if ($sub_array['tanggal_pengiriman'] != NULL) {
+        $sub_array['tanggal_pengiriman'] = Tanggal($sub_array['tanggal_pengiriman']);
+    }
+
+
+    $data[] = $sub_array;
+}
+
+$results = array(
+    "sEcho" => 1,
+        "jumlahRecord" => count($data),
+        "jumlahRecordDitampilkan" => count($data),
+        "data"=>$data);
+
+echo json_encode($results);
+?>
---
+++ users/exim-dan-container/distribusi.php
@@ -0,0 +1,86 @@
+<div class="main-content">
+    <div class="main-content-inner">
+        <div class="breadcrumbs ace-save-state" id="breadcrumbs">
+            <ul class="breadcrumb">
+                <li>
+                    <i class="ace-icon fa fa-home home-icon"></i>
+                    <a href="./">Beranda</a>
+                </li>
+                <li class="active">Distribusi</li>
+            </ul><!-- /.breadcrumb -->
+        </div>
+
+        <div class="page-content">
+
+            <div class="page-header">
+                <h1>
+                    Distribusi
+                    <small>
+                        <i class="ace-icon fa fa-angle-double-right"></i>
+                        Jadwal Distribusi
+                    </small>
+                </h1>
+            </div><!-- /.page-header -->
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <!-- PAGE CONTENT BEGINS -->
+
+                    <div class="clearfix">
+                        <div class="pull-right tableTools-container"></div>
+                    </div>
+                    <div class="table-header">
+                        Daftar data "Distribusi"
+                    </div>
+                    <!-- div.table-responsive -->
+
+                    <!-- div.dataTables_borderWrap -->
+                    <div class="table table-responsive">
+                        <table id="mytable" class="display" width="100%" cellspacing="0">
+                            <thead>
+                                <tr class="">
+                                    <th width="7%" class="text-center">No</th>
+                                    <th width="10%" class="text-left">Nomor Invoice</th>
+                                    <th width="15%" class="text-left">Tanggal Pengiriman</th>
+                                    <th width="15%" class="text-left">Status Persetujuan</th>
+                                    <th width="15%" class="text-left">Status Pengiriman</th>
+                                    <th width="10%" class="text-left"></th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <!-- PAGE CONTENT ENDS -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.page-content -->
+    </div>
+</div><!-- /.main-content -->
+
+<script>
+    // LOADING SCREEN WHILE PROCESS SAVING/UPDATE/DELETE DATA
+    $(document).ready(function(){
+
+        $('#mytable').DataTable({
+                    "bProcessing": true,
+                    "sAjaxSource": "<?php echo $base_url.'action/tampilkan_data_distribusi.php' ?>",
+                    "deferRender": true,
+                    "select": true,
+                    //"dom": 'Bfrtip',
+                    //"scrollY": "300",
+                    //"order": [[ 4, "desc" ]],
+                     "aoColumns": [
+                            { mData: 'no' } ,
+                            { mData: 'nomor_invoice' } ,
+                            { mData: 'tanggal_pengiriman' },
+                            { mData: 'status_persetujuan' } ,
+                            { mData: 'status_pengiriman' },
+                            { mData: 'action' }
+                    ],
+                    "aoColumnDefs": [
+                        { sClass: "dt-center", "aTargets": [0,3,4] },
+                        { sClass: "dt-nowrap", "aTargets": [0,1,2] }
+                    ]
+        });
+
+    });
+</script>
