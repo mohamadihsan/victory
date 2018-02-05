b35d5063d8ee6f7349fae9aca8fec45b301e66b2
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-02-05 10:13:57 +0700

    fixed bug

--- action/peramalan.php
+++ action/peramalan.php
@@ -15,88 +15,102 @@ if(mysqli_escape_string($conn, trim($_POST['hapus']))=='0'){
     $periode_dua_bulan_sebelumnya = date('m-Y', strtotime('-2 month', strtotime($periode_bulan)));
     $alpha      = 0.3;
 
-    // jumlah pemesanan sebulan sebelum periode yang dicari
-    $sql = "SELECT
-            	*
-            FROM
-            	(
-            		SELECT
-            			SUM(quantity) AS jumlah_pemesanan_dua_bulan_kemarin
-            		FROM
-            			detail_pemesanan_produk dpp
-            		LEFT JOIN pemesanan_produk pp ON pp.nomor_invoice = dpp.nomor_invoice
-            		WHERE
-            			dpp.id_produk = 'PR001'
-            		AND DATE_FORMAT(tanggal_pemesanan, '%m-%Y') = '$periode_dua_bulan_sebelumnya'
-            	) AS w
-            JOIN (
-            	SELECT
-            		SUM(quantity) AS jumlah_pemesanan_bulan_kemarin
-            	FROM
-            		detail_pemesanan_produk dpp
-            	LEFT JOIN pemesanan_produk pp ON pp.nomor_invoice = dpp.nomor_invoice
-            	WHERE
-            		dpp.id_produk = 'PR001'
-            	AND DATE_FORMAT(tanggal_pemesanan, '%m-%Y') = '$periode_bulan_sebelumnya'
-            ) AS x
-            JOIN (
-            	SELECT
-            		SUM(quantity) AS jumlah_pemesanan
-            	FROM
-            		detail_pemesanan_produk dpp
-            	LEFT JOIN pemesanan_produk pp ON pp.nomor_invoice = dpp.nomor_invoice
-            	WHERE
-            		dpp.id_produk = 'PR001'
-            	AND DATE_FORMAT(tanggal_pemesanan, '%m-%Y') = '$periode'
-            ) AS y";
+    // jumlah peramalan 1 bulan yang lalu dari periode yang dicari
+    $sql = "SELECT hasil_peramalan, at, bt
+            FROM peramalan
+            WHERE DATE_FORMAT(periode,'%m-%Y') = '$periode_bulan_sebelumnya'";
     $result = mysqli_query($conn, $sql);
-    $data = mysqli_fetch_assoc($result);
-    $jumlah_pemesanan = $data['jumlah_pemesanan'];
-    $jumlah_pemesanan_bulan_kemarin = $data['jumlah_pemesanan_bulan_kemarin'];
-    $jumlah_pemesanan_dua_bulan_kemarin = $data['jumlah_pemesanan_dua_bulan_kemarin'];
-    if ($jumlah_pemesanan_bulan_kemarin == NULL OR $jumlah_pemesanan_dua_bulan_kemarin == NULL) {
-        $hasil_peramalan = 0;
-        $at = 0;
-        $bt = 0;
+    $row = mysqli_fetch_assoc($result);
+    $at = $row['at'];
+    $bt = $row['bt'];
+    if ($bt != 0) {
+
+        // peramalan bulan selanjutnya
+        $hasil_peramalan = ceil($at + $bt);
+
     }else{
-        // cek terlebih dahulu apakah sudah ada data peramalan atau belum
-        // jumlah peramalan 1 bulan yang lalu dari periode yang dicari
-        $sql = "SELECT hasil_peramalan, at, bt
-                FROM peramalan
-                WHERE DATE_FORMAT(periode,'%m-%Y') = '$periode_bulan_sebelumnya'";
+        // jumlah pemesanan sebulan sebelum periode yang dicari
+        $sql = "SELECT
+                	*
+                FROM
+                	(
+                		SELECT
+                			SUM(quantity) AS jumlah_pemesanan_dua_bulan_kemarin
+                		FROM
+                			detail_pemesanan_produk dpp
+                		LEFT JOIN pemesanan_produk pp ON pp.nomor_invoice = dpp.nomor_invoice
+                		WHERE
+                			dpp.id_produk = 'PR001'
+                		AND DATE_FORMAT(tanggal_pemesanan, '%m-%Y') = '$periode_dua_bulan_sebelumnya'
+                	) AS w
+                JOIN (
+                	SELECT
+                		SUM(quantity) AS jumlah_pemesanan_bulan_kemarin
+                	FROM
+                		detail_pemesanan_produk dpp
+                	LEFT JOIN pemesanan_produk pp ON pp.nomor_invoice = dpp.nomor_invoice
+                	WHERE
+                		dpp.id_produk = 'PR001'
+                	AND DATE_FORMAT(tanggal_pemesanan, '%m-%Y') = '$periode_bulan_sebelumnya'
+                ) AS x
+                JOIN (
+                	SELECT
+                		SUM(quantity) AS jumlah_pemesanan
+                	FROM
+                		detail_pemesanan_produk dpp
+                	LEFT JOIN pemesanan_produk pp ON pp.nomor_invoice = dpp.nomor_invoice
+                	WHERE
+                		dpp.id_produk = 'PR001'
+                	AND DATE_FORMAT(tanggal_pemesanan, '%m-%Y') = '$periode'
+                ) AS y";
         $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_assoc($result);
-        $at = $row['at'];
-        $bt = $row['bt'];
-        if ($bt != 0) {
-
-            // peramalan bulan selanjutnya
-            $hasil_peramalan = ceil($at + $bt);
-            echo $periode_dua_bulan_sebelumnya;die();
-
+        $data = mysqli_fetch_assoc($result);
+        $jumlah_pemesanan = $data['jumlah_pemesanan'];
+        $jumlah_pemesanan_bulan_kemarin = $data['jumlah_pemesanan_bulan_kemarin'];
+        $jumlah_pemesanan_dua_bulan_kemarin = $data['jumlah_pemesanan_dua_bulan_kemarin'];
+        if ($jumlah_pemesanan_bulan_kemarin == NULL OR $jumlah_pemesanan_dua_bulan_kemarin == NULL) {
+            $hasil_peramalan = 0;
+            $at = 0;
+            $bt = 0;
         }else{
-            // lakukan dari awal
-            $omzet_dua_bulan_kemarin = $jumlah_pemesanan_dua_bulan_kemarin;
-            $exp_smoothing_dua_bulan_kemarin = $jumlah_pemesanan_dua_bulan_kemarin;
-            $double_exp_smoothing_dua_bulan_kemarin = $jumlah_pemesanan_dua_bulan_kemarin;
-            $at_dua_bulan_kemarin = $jumlah_pemesanan_dua_bulan_kemarin;
-            $bt_dua_bulan_kemarin = $exp_smoothing_dua_bulan_kemarin - $double_exp_smoothing_dua_bulan_kemarin;
+            // cek terlebih dahulu apakah sudah ada data peramalan atau belum
+            // jumlah peramalan 1 bulan yang lalu dari periode yang dicari
+            $sql = "SELECT hasil_peramalan, at, bt
+                    FROM peramalan
+                    WHERE DATE_FORMAT(periode,'%m-%Y') = '$periode_bulan_sebelumnya'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_assoc($result);
+            $at = $row['at'];
+            $bt = $row['bt'];
+            if ($bt != 0) {
 
-            // count single exponential
-            $omzet_bulan_kemarin = $jumlah_pemesanan_bulan_kemarin;
-            $exp_smoothing_bulan_kemarin = ($alpha * $omzet_bulan_kemarin) + (1-$alpha) * $exp_smoothing_dua_bulan_kemarin;
+                // peramalan bulan selanjutnya
+                $hasil_peramalan = ceil($at + $bt);
 
-            // count double exponential
-            $double_exp_smoothing_bulan_kemarin = ($alpha * $exp_smoothing_bulan_kemarin) + (1-$alpha) * $double_exp_smoothing_dua_bulan_kemarin;
+            }else{
+                // lakukan dari awal
+                $omzet_dua_bulan_kemarin = $jumlah_pemesanan_dua_bulan_kemarin;
+                $exp_smoothing_dua_bulan_kemarin = $jumlah_pemesanan_dua_bulan_kemarin;
+                $double_exp_smoothing_dua_bulan_kemarin = $jumlah_pemesanan_dua_bulan_kemarin;
+                $at_dua_bulan_kemarin = $jumlah_pemesanan_dua_bulan_kemarin;
+                $bt_dua_bulan_kemarin = $exp_smoothing_dua_bulan_kemarin - $double_exp_smoothing_dua_bulan_kemarin;
 
-            // count at
-            $at = 2 * $exp_smoothing_bulan_kemarin - $double_exp_smoothing_bulan_kemarin;
+                // count single exponential
+                $omzet_bulan_kemarin = $jumlah_pemesanan_bulan_kemarin;
+                $exp_smoothing_bulan_kemarin = ($alpha * $omzet_bulan_kemarin) + (1-$alpha) * $exp_smoothing_dua_bulan_kemarin;
 
-            // count bt
-            $bt = ($alpha / (1 - $alpha)) * ($exp_smoothing_bulan_kemarin - $double_exp_smoothing_bulan_kemarin);
+                // count double exponential
+                $double_exp_smoothing_bulan_kemarin = ($alpha * $exp_smoothing_bulan_kemarin) + (1-$alpha) * $double_exp_smoothing_dua_bulan_kemarin;
 
-            // peramalan bulan selanjutnya
-            $hasil_peramalan = ceil($at + $bt);
+                // count at
+                $at = 2 * $exp_smoothing_bulan_kemarin - $double_exp_smoothing_bulan_kemarin;
+
+                // count bt
+                $bt = ($alpha / (1 - $alpha)) * ($exp_smoothing_bulan_kemarin - $double_exp_smoothing_bulan_kemarin);
+
+                // peramalan bulan selanjutnya
+                $hasil_peramalan = ceil($at_dua_bulan_kemarin + $bt_dua_bulan_kemarin);
+            }
         }
     }
 
