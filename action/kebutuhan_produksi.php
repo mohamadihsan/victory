4b6892fd616ffb74219fd1a3dad30cb50c35035a
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-02-13 10:33:14 +0700

    kebutuhan produksi

---
+++ action/kebutuhan_produksi.php
@@ -0,0 +1,85 @@
+<?php
+// buka koneksi
+require_once '../config/connection.php';
+
+$id_kebutuhan_produksi      = mysqli_escape_string($conn, trim($_POST['id_kebutuhan_produksi']));
+if(mysqli_escape_string($conn, trim($_POST['hapus']))=='0'){
+    $id_kebutuhan_produksi    = mysqli_escape_string($conn, trim($_POST['id_kebutuhan_produksi']));
+    $id_produk    = mysqli_escape_string($conn, trim($_POST['id_produk']));
+    $quantity_produksi       = mysqli_escape_string($conn, trim($_POST['quantity_produksi']));
+    $tanggal   = mysqli_escape_string($conn, trim($_POST['tanggal']));
+    $status        = mysqli_escape_string($conn, trim($_POST['status']));
+}
+
+
+if ($id_kebutuhan_produksi=='') {
+    
+    // simpan data
+    $sql = "INSERT INTO kebutuhan_produksi (id_kebutuhan_produksi, id_produk, quantity_produksi, tanggal, status)
+            VALUES ('$id_kebutuhan_produksi', '$id_produk', '$quantity_produksi', '$tanggal', '$status')";
+    if(mysqli_query($conn, $sql)){
+        $pesan_berhasil = "Data berhasil disimpan";
+    }else{
+        $pesan_gagal = "Data gagal disimpan";
+    }
+}else if($id_kebutuhan_produksi!='' AND empty(mysqli_escape_string($conn, trim($_POST['hapus'])))){
+    // perbaharui data
+    $sql = "UPDATE kebutuhan_produksi
+            SET id_kebutuhan_produksi='$id_kebutuhan_produksi', id_produk='$id_produk', quantity_produksi='$quantity_produksi', tanggal='$tanggal', status='$status'
+            WHERE id_kebutuhan_produksi='$id_kebutuhan_produksi'";
+    if(mysqli_query($conn, $sql)){
+        $pesan_berhasil = "Data berhasil diperbaharui";
+    }else{
+        $pesan_gagal = "Data gagal diperbaharui";
+    }
+}else if(mysqli_escape_string($conn, trim($_POST['hapus']))=='1'){
+    // hapus data
+    $sql = "DELETE FROM kebutuhan_produksi
+            WHERE id_kebutuhan_produksi='$id_kebutuhan_produksi'";
+    if(mysqli_query($conn, $sql)){
+        $pesan_berhasil = "Data berhasil dihapus";
+    }else{
+        $pesan_gagal = "Data gagal dihapus";
+    }
+}
+
+if (isset($pesan_berhasil)) {
+    ?>
+    <script type="text/javascript">
+        $(function(){
+            $.gritter.add({
+                // (string | mandatory) the heading of the notification
+                title: 'Sukses!',
+                // (string | mandatory) the text inside the notification
+                text: '<?= $pesan_berhasil ?>',
+                // (string | optional) the image to display on the left
+                image: '../assets/images/berhasil.png',
+                // (bool | optional) if you want it to fade out on its own or just sit there
+                sticky: false,
+                // (int | optional) the time you want it to be alive for before fading out
+                time: ''
+            });
+        });
+    </script>
+    <?php
+}else if(isset($pesan_gagal)){
+    ?>
+    <script type="text/javascript">
+	    $(function(){
+            $.gritter.add({
+                // (string | mandatory) the heading of the notification
+                title: 'Gagal!',
+                // (string | mandatory) the text inside the notification
+                text: '<?= $pesan_gagal ?>',
+                // (string | optional) the image to display on the left
+                image: '../assets/images/gagal.png',
+                // (bool | optional) if you want it to fade out on its own or just sit there
+                sticky: false,
+                // (int | optional) the time you want it to be alive for before fading out
+                time: ''
+	        });
+        });
+	</script>
+    <?php
+}
+?>
---
+++ action/tampilkan_data_kebutuhan_produksi.php
@@ -0,0 +1,36 @@
+<?php
+// buka koneksi
+require_once '../config/connection.php';
+
+// sql statement
+$sql = "SELECT id_kebutuhan_produksi, p.id_produk, p.style, nama_produk, tanggal, quantity_produksi, tanggal, status
+            FROM kebutuhan_produksi kp
+        LEFT JOIN produk p ON p.id_produk = kp.id_produk
+        ORDER BY 1 DESC";
+$result = mysqli_query($conn, $sql);
+$data = array();
+$no = 1;
+while ($row = mysqli_fetch_assoc($result)) {
+    $sub_array['no']                = $no++;
+    $sub_array['id_kebutuhan_produksi']     = $row['id_kebutuhan_produksi'];
+    $sub_array['id_produk']         = $row['id_produk'];
+    $sub_array['nama_produk']         = $row['nama_produk'];
+    $sub_array['style']         = $row['style'];
+    $sub_array['tanggal']            = $row['tanggal'];
+    $sub_array['quantity_produksi']        = $row['quantity_produksi'];
+    $sub_array['tanggal']             = $row['tanggal'];
+    $sub_array['status']             = $row['status'];
+	  $sub_array['action']		        = ' <button type="button" class="btn btn-warning btn-xs" data-toggle="collapse" data-target=".tampil" onclick="return ubah(\''.$row['id_kebutuhan_produksi'].'\',\''.$row['id_produk'].'\',\''.$row['nama_produk'].'\',\''.$row['tanggal'].'\',\''.$row['quantity_produksi'].'\',\''.$row['tanggal'].'\',\''.$row['status'].'\')"><i class="ace-icon fa fa-pencil-square-o bigger-120"></i> </button>
+                                        <button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#hapus" onclick="return hapus(\''.$row['id_kebutuhan_produksi'].'\')"><i class="ace-icon fa fa-trash-o bigger-120"></i> </button>';
+
+    $data[] = $sub_array;
+}
+
+$results = array(
+    "sEcho" => 1,
+        "jumlahRecord" => count($data),
+        "jumlahRecordDitampilkan" => count($data),
+        "data"=>$data);
+
+echo json_encode($results);
+?>
